b506ebbdffd7cbaca4f5d684b8b570ec9fb2d6ea
gismarb <gismar_barbosa@yahoo.com.br>
2019-04-10 09:46:37 -0300

    Add Pasta Alunos c/ subpastas (nomes de cada aluno)

--- aula05/alunos/gismar/AppMonitor.cpp
+++ aula05/alunos/gismar/AppMonitor.cpp
@@ -4,3 +4,18 @@
  *
  *
  * */
+
+#include <iostream>
+#include <string.h>
+#include "Ponto.cpp"
+#include "ClasseMonitor.cpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    Monitor monitor1;
+    Ponto minimo;
+    Ponto maximo;
+
+    return 0;
+}
